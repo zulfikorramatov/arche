2246c4bb34009600546d97a6333c4b8d6ba41d85
Zulfikor Ramatov <zulfikor.r@tbcbank.uz>
2026-06-30 21:08:26 +0500

    kafka

---
+++ pkg/kafka/options.go
@@ -0,0 +1,31 @@
+package kafka
+
+import "time"
+
+type kafkaOptions struct {
+	username     string
+	password     string
+	readTimeout  time.Duration
+	writeTimeout time.Duration
+}
+
+type Option func(*kafkaOptions)
+
+func SASLAuth(username, password string) Option {
+	return func(o *kafkaOptions) {
+		o.username = username
+		o.password = password
+	}
+}
+
+func ReadTimeout(timeout time.Duration) Option {
+	return func(o *kafkaOptions) {
+		o.readTimeout = timeout
+	}
+}
+
+func WriteTimeout(timeout time.Duration) Option {
+	return func(o *kafkaOptions) {
+		o.writeTimeout = timeout
+	}
+}
